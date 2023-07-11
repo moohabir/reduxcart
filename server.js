9e9e4e3df0dbb1de991cc0764316913d819dd421
moohabir <moohabir@gmail.com>
2023-07-12 00:09:10 +0200

    dfg

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get('/config', (req, res) => {
+  res.send({ pablishebaleKey: process.env.STRIPE_PUBLISHABLE_KEY });
+});
+
+app.post('/create-payment-intent', async (req, res) => {
+  try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      currency: 'EUR',
+      amount: 1999,
+      automatic_payment_methods: {
+        enabled: true,
+      },
+    });
+    res.send({ client_secret: paymentIntent.client_secret });
+  } catch (e) {
+    return res.status(400).send({ error: { message: e.message } });
+  }
+});
+
+app.listen(process.env.PORT, (req, res) => {
+  console.log(`Backend connected succussfully at port ${process.env.PORT}`);
+});
