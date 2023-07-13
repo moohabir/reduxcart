c626f14c8cff47af5aa51a98c4f6d9e8e4c74c29
moohabir <moohabir@gmail.com>
2023-07-14 00:13:59 +0200

    tttt

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ if (process.env.NODE_ENV === 'production') {
 
   // Route all other requests to the frontend's "index.html" file
   app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, './frontend/build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, './client/build', 'index.html'))
   );
 } else {
   app.get('/', (req, res) => res.send('Please set to production'));
